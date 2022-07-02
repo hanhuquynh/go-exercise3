381e451dbdcfcbc6a45fcbd2bedc0e5f69968c42
Quỳnh <hanhuquynh25122001@gmail.com>
2022-07-02 16:49:26 +0700

    exercises3

--- bai1.go
+++ bai1.go
@@ -2,23 +2,51 @@ package main
 
 import (
 	"log"
+	"sync"
 	"time"
+)
+
+var (
+	mu sync.Mutex
+	wg sync.WaitGroup
 )
 
 func chanRoutine() {
 	log.Print("hello 1")
+	wg.Add(1)
 	go func() {
 		time.Sleep(1 * time.Second)
 		log.Print("hello 3")
+		wg.Done()
 	}()
 	log.Print("hello 2")
+	wg.Wait()
 }
 
 func chanRoutineMuTex() {
+
+	log.Print("hello 1")
+	mu.Lock()
+	wg.Add(1)
+	go func() {
+		time.Sleep(1 * time.Second)
+		log.Print("hello 3")
+		wg.Done()
+		mu.Unlock()
+	}()
+	mu.Lock()
+	log.Print("hello 2")
+	wg.Wait()
+}
+
+func chanRoutineChannel() {
+	done := make(chan int, 1)
 	log.Print("hello 1")
 	go func() {
 		time.Sleep(1 * time.Second)
 		log.Print("hello 3")
+		done <- 1
 	}()
+	<-done
 	log.Print("hello 2")
 }
--- main.go
+++ main.go
@@ -1,6 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
+	fmt.Println("WaitGroup")
 	chanRoutine()
+
+	fmt.Println("--------------------")
+
+	fmt.Println("Mutex")
 	chanRoutineMuTex()
+
+	fmt.Println("--------------------")
+
+	fmt.Println("Channel")
+	chanRoutineChannel()
 }
